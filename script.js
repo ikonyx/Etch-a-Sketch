424f24da697be5bfe6b532a8d49e1400e3a48550
ikram <150953076+ikonyx@users.noreply.github.com>
2024-09-06 14:53:19 +0100

    Add button to change grid size

--- script.js
+++ script.js
@@ -11,19 +11,42 @@ function createGrid() {
             square.style.height = (containerSize/gridSize) +'px';
             container.appendChild(square);
         } 
-    }  
+    } 
+
+    const squares = document.querySelectorAll('div.squares');
+
+    for (const square of squares) {
+         square.addEventListener('mouseenter', pickColor)
+    }
+    
+    function pickColor(event) {
+    event.target.style.backgroundColor = color.value;
+    };
+
 };
 
 createGrid();
 
-const squares = document.querySelectorAll('div.squares');
 
-for (const square of squares) {
-    square.addEventListener('mouseenter', pickColor)
-  }
-function pickColor(event) {
-    event.target.style.backgroundColor = color.value;
-};
+const GridSizeBtn  = document.createElement('button');
+GridSizeBtn.textContent = 'Grid Size';
+buttons.appendChild(GridSizeBtn);
 
+GridSizeBtn.addEventListener('click', changeGridSize);
 
+function changeGridSize() {
     
+    let newGridSize
+    while (true) { 
+        newGridSize = parseInt(prompt("Please enter a number between 2 and 100"));
+        
+        if (!isNaN(newGridSize) && 2 <= newGridSize && newGridSize <= 100) {
+            gridSize = newGridSize;
+            document.querySelector('#grid').replaceChildren();
+            createGrid();
+            break;
+        } else if (newGridSize === null) {
+            break;
+        }     
+    }
+}
